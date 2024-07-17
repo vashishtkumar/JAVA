5b81829a6bebfb3475af329386a0d58f9081d55f
vashishtkumar <vashishtk270@gmail.com>
2024-07-18 00:19:16 +0530

    Array

---
+++ Array/src/LargestOfArrayUsingLinearSearch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LargestOfArrayUsingLinearSearch {
+
+    public static int Largest(int arr[]){
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(Largest(arr));
+
+
+
+    }
+}
